b5fee46fc5afd21d8762c6d8de43df4e04141e9d
Colin Beltz <colinbeltz@gmail.com>
2023-09-10 11:54:29 -0500

    Completed Lesson 19-4 Header and Footer styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,9 @@
+import { Link } from "react-router-dom"
+
 export const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <p>© 2030 <Link to="/">WriteNode</Link>. All Rights Reserved.</p>
+    </footer>
   )
 }
