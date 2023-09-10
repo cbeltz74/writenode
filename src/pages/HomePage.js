8dac711b04547f514f25e4a0f1491d968df72598
Colin Beltz <colinbeltz@gmail.com>
2023-09-10 14:45:20 -0500

    Completed 19-5 Homepage. Finished HomePage.js, PostCard.js, CreatePost.js and PageNotFound.js

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,16 @@
+import { PostCard } from "../components";
+
+const posts = [
+  {id: 1, title: "Lorem ipsum dolor sit amet", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda quibusdam cupiditate minus molestias! Unde est consequatur dignissimos, quae nostrum cumque recusandae laboriosam deserunt temporibus magnam! Voluptas modi ullam, dolor temporibus mollitia alias minima officiis doloribus quis sunt eos. Doloribus tenetur distinctio esse qui natus reiciendis eaque est quidem illo voluptatum!", author: "Colin" },
+  {id: 2, title: "Lorem ipsum dolor sit amet", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda quibusdam cupiditate minus molestias! Unde est consequatur dignissimos, quae nostrum cumque recusandae laboriosam deserunt temporibus magnam! Voluptas modi ullam, dolor temporibus mollitia alias minima officiis doloribus quis sunt eos. Doloribus tenetur distinctio esse qui natus reiciendis eaque est quidem illo voluptatum!", author: "Thomas" }
+]
+
 export const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <section>
+      { posts.map(post => (
+        <PostCard key={post.id} post={post}/>
+      ))}
+    </section>
   )
 }
--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,5 +1,14 @@
+import { Link } from "react-router-dom";
+import image from "../assets/images/page-not-found.jpg";
+
 export const PageNotFound = () => {
   return (
-    <div>PageNotFound</div>
-  )
+    <section className="pageNotFound">
+      <p>404 / Page Not Found</p>
+      <img src={image} alt="Page not found" />
+      <Link to="/">
+        <button>Back to Home</button>
+      </Link>
+    </section>
+    )
 }
