4e4c906d778923e46bbff663a19178bee7e3888b
Colin Beltz <colinbeltz@gmail.com>
2023-09-10 15:17:03 -0500

    Completed Lesson 19, Step 6 Setting up Firebase project

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAYOb1GF4mto3X_6w1XGyqj8OimoYhbBJw",
+  authDomain: "writenode-cb.firebaseapp.com",
+  projectId: "writenode-cb",
+  storageBucket: "writenode-cb.appspot.com",
+  messagingSenderId: "76201584954",
+  appId: "1:76201584954:web:cb344b2b9b170fd045c803"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
